1f5c0653227db82f354cf38626da0019d67a43ff
Luke K <lukekaicher00@gmail.com>
2023-07-27 17:53:57 -0400

    started updating bark script, does not work yet with ffmpeg conversions

--- Dialog/bark-generate-voices.py
+++ Dialog/bark-generate-voices.py
@@ -2,9 +2,12 @@ import csv
 import os
 import sys
 import torch
+import ffmpeg
 from bark import SAMPLE_RATE, generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 from IPython.display import Audio
+
+os.chdir(os.path.dirname(sys.argv[0]))
 
 PATH = "../Assets/Audio/Resources/Voice/"
 
@@ -17,26 +20,21 @@ else:
 os.environ["SUNO_ENABLE_MPS"] = "True"
 preload_models()
 
-
-# generate audio from text
-
-  
-# play text in notebook
-#Audio(audio_array, rate=SAMPLE_RATE)
-
-
-
-
-# os.chdir(os.path.dirname(sys.argv[0]))
-
 charMap={"Narr":"en_speaker_9","Dave":"en_speaker_4","Tony":"en_speaker_1","Neighbor2":"it_speaker_en",'HardwareClerk':"hi_speaker_5"}
 
-# phaseMap={"English":"","PhaseOne":"P1/", "PhaseTwo":"P2/"}
-input_file = csv.DictReader(open("CHFDialog.csv"))
+phaseMap={"English":"","PhaseOne":"P1/", "PhaseTwo":"P2/"}
+input_file = csv.DictReader(open("CHFDialogTest.csv"))
 for row in input_file:
   bias = "MAN"
   if (row["Character"] == "Narr"):
     bias = "WOMAN"
   audio_array = generate_audio(f'[{bias}] {row["English"]}', history_prompt=f'v2/{charMap[row["Character"]]}')
-  write_wav(f'{PATH}{row["Character"]}{row["ID"]}.wav', SAMPLE_RATE, audio_array)
+  filename = f'{PATH}{row["Character"]}{row["ID"]}'
+  write_wav(filename+'.wav', SAMPLE_RATE, audio_array)
+
+  input = ffmpeg.input(filename+'.wav')
+  audio = input.audio.filter('volume', 2)
+  out = ffmpeg.output(audio, filename+'.ogg')
+
+  # os.remove(filename+'.wav')
 
