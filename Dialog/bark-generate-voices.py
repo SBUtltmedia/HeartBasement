d1a2c7cec7b526bc99e561be6b603cf6ca3840c4
Paul St. Denis <pstdenis@bunsen-tll.labs.stonybrook.edu>
2023-09-08 17:52:11 -0400

    better bark audio

--- Dialog/bark-generate-voices.py
+++ Dialog/bark-generate-voices.py
@@ -13,7 +13,7 @@ from scipy.io.wavfile import write as write_wav
 from IPython.display import Audio
 os.chdir(os.path.dirname(sys.argv[0]))
 silence = np.zeros(int(0.5 * SAMPLE_RATE)) 
-GEN_TEMP = 0.7
+GEN_TEMP = 1
 RUN_BARK=False
 RUN_BARK=True
 PATH = "../Assets/Audio/Resources/Voice/"
@@ -27,7 +27,7 @@ if RUN_BARK:
 	os.environ["SUNO_ENABLE_MPS"] ="true" 
 	preload_models()
 
-charMap={"Narr":"en_speaker_9","Dave":"en_speaker_6","Tony":"en_speaker_1","Neighbor2":"it_speaker_en",'HardwareClerk':"en_speaker_4"}
+charMap={"Narr":"en_speaker_9","Dave":"en_speaker_6","Tony":"en_speaker_0","Neighbor2":"it_speaker_en",'HardwareClerk':"en_speaker_4"}
 phaseMap={"English":"","PhaseOne":"P1/", "PhaseTwo":"P2/"}
 input_file = csv.DictReader(open("CHFDialog.csv"))
 for row in input_file:
@@ -36,19 +36,23 @@ for row in input_file:
 	if (row["Character"] == "Narr"):
 		bias = ""
 	sentence = f'{row["English"]}'.strip()
-	print("*",sentence,"*")
-	semantic_tokens = generate_text_semantic(
-        sentence,
-        history_prompt=speaker,
-        temp=GEN_TEMP,
-        min_eos_p=0.05,  # this controls how likely the generation is to end
-    )
-	
 	filename = f'{PATH}{row["Character"]}{row["ID"]}'
 	if RUN_BARK and row["Character"]=="Tony":
+		print("*",sentence,"*")
+		semantic_tokens = generate_text_semantic(
+			sentence,
+			history_prompt=speaker,
+			temp=GEN_TEMP,
+			min_eos_p=0.05,  # this controls how likely the generation is to end
+		)
+		
+	
+		
 		audio_array = semantic_to_waveform(semantic_tokens, history_prompt=speaker)
 		write_wav(filename+'.wav', SAMPLE_RATE, audio_array)
 
 	os.system(f'ffmpeg -y -i {filename}.wav -filter:a "volume=2" {filename}.ogg')
-	os.remove(filename+'.wav')
-
+	try:
+		os.remove(filename+'.wav')
+	except:
+		print(f'{filename} missing' )
