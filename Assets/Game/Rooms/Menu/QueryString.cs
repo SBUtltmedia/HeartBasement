5f01d05fe6cd7025f702a9a61060146191bba3b3
Luke Kaicher <luke.kaicher@stonybrook.edu>
2024-05-20 19:05:24 -0400

    move querystring.cs code to onshow menu screen, new build, query for test mode works

--- Assets/Game/Rooms/Menu/QueryString.cs
+++ Assets/Game/Rooms/Menu/QueryString.cs
@@ -2,19 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.InteropServices;
+
+using PowerTools.Quest;
+using PowerScript;
+using static GlobalScript;
 public class QueryString : MonoBehaviour
 {
-           [DllImport("__Internal")]
+    [DllImport("__Internal")]
     private static extern string getQueryString();
     // Start is called before the first frame update
     void Start()
     {
       //Debug.Log(getQueryString());
-      Globals.urlQuery = getQueryString();
-      Debug.Log(Globals.urlQuery); 
-      if (Globals.urlQuey.contains("test")){
-        Globals.testflag = true;
-      }
+
     }
 
     // Update is called once per frame
--- Assets/Game/Rooms/Menu/RoomMenu.cs
+++ Assets/Game/Rooms/Menu/RoomMenu.cs
@@ -3,36 +3,62 @@ using System.Collections;
 using PowerTools.Quest;
 using PowerScript;
 using static GlobalScript;
+using System;
+
+using System.Runtime.InteropServices;
+
 
 public class RoomMenu : RoomScript<RoomMenu>
 {
 
+	// QueryString Code
+	[DllImport("__Internal")]
+	private static extern string getQueryString();
+
+
 
 	void OnEnterRoom()
 	{
+		
+		// QueryString Code
+		try {
+		Globals.urlQuery = getQueryString();
+		
+		Debug.Log(Globals.urlQuery);
+		if (Globals.urlQuery.Contains("test"))
+		{
+			Globals.testflag = true;
+		}
+		} catch (Exception e) {}
+
+
 		Audio.PlayMusic("Menu2", 1);
 		//G.Inventory.Hide();
 		G.MenuButton.Hide();
 		G.TitleMenu.Show();
+
+
+
+
 	}
 
-	IEnumerator OnExitRoom( IRoom oldRoom, IRoom newRoom )
+	IEnumerator OnExitRoom(IRoom oldRoom, IRoom newRoom)
 	{
 		G.TitleMenu.Hide();
 		G.ChapterSelect.Hide();
 		G.MenuButton.Show();
 		//G.Inventory.Show();
-		
+
 		yield return E.Break;
 	}
 
-	IEnumerator OnInteractCharacterTony( ICharacter character )
+	IEnumerator OnInteractCharacterTony(ICharacter character)
 	{
 
 		yield return E.Break;
 	}
 
-	IEnumerator OnUseInvCharacterTony( ICharacter character, IInventory item )
+	IEnumerator OnUseInvCharacterTony(ICharacter character, IInventory item)
 	{
 
 		yield return E.Break;
