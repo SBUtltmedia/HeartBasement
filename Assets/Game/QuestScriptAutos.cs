18c4c1faa6f39d3be582cbe25f735fb2a91ca3e8
Luke Kaicher <luke.kaicher@stonybrook.edu>
2025-01-17 18:56:34 -0500

    compress hardware store and water sprites

--- Assets/Game/QuestScriptAutos.cs
+++ Assets/Game/QuestScriptAutos.cs
@@ -64,7 +64,6 @@ namespace PowerScript
 		public static IRoom Home		{ get{return PowerQuest.Get.GetRoom("Home"); } }
 		public static IRoom Map		{ get{return PowerQuest.Get.GetRoom("Map"); } }
 		public static IRoom Hardware		{ get{return PowerQuest.Get.GetRoom("Hardware"); } }
-		public static IRoom Cutscene		{ get{return PowerQuest.Get.GetRoom("Cutscene"); } }
 		public static IRoom Menu           { get { return PowerQuest.Get.GetRoom("Menu"); } }
 		public static IRoom NewHouse       { get { return PowerQuest.Get.GetRoom("NewHouse"); } }
 		// #ROOM# - Do not edit this line, it's used by the system to insert rooms for easy access
